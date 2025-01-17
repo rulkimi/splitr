312d1bec9d74cddeab31f4a77c8f925109f71f30
rulkimi <azrulkimi00@gmail.com>
2025-01-17 18:07:51 +0800

    remove unnecessary code

--- client/src/components/bill-details.tsx
+++ client/src/components/bill-details.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import {
   Table,
   TableBody,
@@ -9,7 +9,6 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
-import { useEffect, useState } from "react";
 
 const BillDetails = ({ receipt }: { receipt: Receipt }) => {
   return (
